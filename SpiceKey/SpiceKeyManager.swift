f62f45af6d780ed826440ff841a2102b554f1af2
Kyome22 <kyomesuke@gmail.com>
2019-06-01 17:49:16 +0900

    長押しの修正

--- SpiceKey/SpiceKeyManager.swift
+++ SpiceKey/SpiceKeyManager.swift
@@ -9,14 +9,14 @@
 import AppKit
 import Carbon
 
-private let LEFT_CONTROL:  Int64 = 0x3B
-private let RIGHT_CONTROL: Int64 = 0x3E
-private let LEFT_OPTION:   Int64 = 0x3A
-private let RIGHT_OPTION:  Int64 = 0x3D
-private let LEFT_SHIFT:    Int64 = 0x38
-private let RIGHT_SHIFT:   Int64 = 0x3C
-private let LEFT_COMMAND:  Int64 = 0x37
-private let RIGHT_COMMAND: Int64 = 0x36
+fileprivate let LEFT_CONTROL:  UInt16 = 0x3B
+fileprivate let RIGHT_CONTROL: UInt16 = 0x3E
+fileprivate let LEFT_OPTION:   UInt16 = 0x3A
+fileprivate let RIGHT_OPTION:  UInt16 = 0x3D
+fileprivate let LEFT_SHIFT:    UInt16 = 0x38
+fileprivate let RIGHT_SHIFT:   UInt16 = 0x3C
+fileprivate let LEFT_COMMAND:  UInt16 = 0x37
+fileprivate let RIGHT_COMMAND: UInt16 = 0x36
 
 final class SpiceKeyManager {
     
@@ -24,6 +24,7 @@ final class SpiceKeyManager {
     internal var spiceKeys = [UUID : SpiceKey]()
     private let signature = UTGetOSTypeFromString("SpiceKey" as CFString)
     private var count: UInt32 = 0
+    private var monitors = [Any?]()
     private var keyFlag: (left: Bool, right: Bool) = (false, false)
     private var modifierFlags: ModifierFlags = .empty
     private var workItem: DispatchWorkItem? = nil
@@ -39,23 +40,24 @@ final class SpiceKeyManager {
                             eventHandleNegotiator,
                             2, eventSpecs, nil, nil)
         
-        // event observation
-        if let event = CGEvent.tapCreate(tap: CGEventTapLocation.cghidEventTap,
-                                         place: CGEventTapPlacement.headInsertEventTap,
-                                         options: CGEventTapOptions.listenOnly,
-                                         eventsOfInterest: CGEventMask(1 << CGEventType.flagsChanged.rawValue),
-                                         callback: modifiersEventHandleNegotiator,
-                                         userInfo: nil) {
-            let source = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, event, 0)
-            CFRunLoopAddSource(CFRunLoopGetCurrent(), source, CFRunLoopMode.commonModes)
-            CGEvent.tapEnable(tap: event, enable: true)
-        } else {
-            Swift.print("Failed to initialize SpiceKeyManager")
+        let local = NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) { (event) -> NSEvent? in
+            let flags = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+            SpiceKeyManager.shared.catchModifersEvent(event.keyCode, flags)
+            return event
         }
+        monitors.append(local)
+        let global = NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged) { (event) in
+            let flags = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+            SpiceKeyManager.shared.catchModifersEvent(event.keyCode, flags)
+        }
+        monitors.append(global)
     }
     
     deinit {
-        // (๑╹ω╹๑ )??
+        for monitor in monitors {
+            NSEvent.removeMonitor(monitor!)
+        }
+        monitors.removeAll()
     }
     
     func register(_ spiceKey: SpiceKey) {
@@ -142,11 +144,11 @@ final class SpiceKeyManager {
         return OSStatus(eventNotHandledErr)
     }
     
-    private func checkFlags(_ event: CGEvent) -> (ctr: Bool, opt: Bool, sft: Bool, cmd: Bool) {
-        let ctr = event.flags.contains(CGEventFlags.maskControl)
-        let opt = event.flags.contains(CGEventFlags.maskAlternate)
-        let sft = event.flags.contains(CGEventFlags.maskShift)
-        let cmd = event.flags.contains(CGEventFlags.maskCommand)
+    private func checkFlags(_ flags: NSEvent.ModifierFlags) -> (ctr: Bool, opt: Bool, sft: Bool, cmd: Bool) {
+        let ctr = flags.contains(.control)
+        let opt = flags.contains(.option)
+        let sft = flags.contains(.shift)
+        let cmd = flags.contains(.command)
         let ctr_ = ctr && !(opt || sft || cmd)
         let opt_ = opt && !(ctr || sft || cmd)
         let sft_ = sft && !(ctr || opt || cmd)
@@ -154,10 +156,9 @@ final class SpiceKeyManager {
         return (ctr_, opt_, sft_, cmd_)
     }
     
-    func catchModifersEvent(_ event: CGEvent) -> Unmanaged<CGEvent>? {
+    func catchModifersEvent(_ keyCode: UInt16, _ flags: NSEvent.ModifierFlags) {
         // both side
-        let keyCode = event.getIntegerValueField(CGEventField.keyboardEventKeycode)
-        let flag = checkFlags(event)
+        let flag = checkFlags(flags)
         if flag.ctr || flag.opt || flag.sft || flag.cmd {
             var modifierFlag: ModifierFlag!
             if flag.ctr {
@@ -193,10 +194,10 @@ final class SpiceKeyManager {
         
         // long tap
         workItem?.cancel()
-        let modifierFlags = ModifierFlags(control: event.flags.contains(CGEventFlags.maskControl),
-                                          option:  event.flags.contains(CGEventFlags.maskAlternate),
-                                          shift:   event.flags.contains(CGEventFlags.maskShift),
-                                          command: event.flags.contains(CGEventFlags.maskCommand))
+        let modifierFlags = ModifierFlags(control: flags.contains(.control),
+                                          option:  flags.contains(.option),
+                                          shift:   flags.contains(.shift),
+                                          command: flags.contains(.command))
         let longPressSpiceKeys = spiceKeys.values.filter { (spiceKey) -> Bool in
             return spiceKey.interval > 0.0 && spiceKey.modifierFlags! == modifierFlags
         }
@@ -206,7 +207,6 @@ final class SpiceKeyManager {
             })
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + spiceKey.interval, execute: workItem!)
         }
-        return Unmanaged.passRetained(event)
     }
     
 }
@@ -215,11 +215,4 @@ private func eventHandleNegotiator(eventHandlerCall: EventHandlerCallRef?,
                                    event: EventRef?,
                                    userData: UnsafeMutableRawPointer?) -> OSStatus {
     return SpiceKeyManager.shared.handleEvent(event)
-}
-
-private func modifiersEventHandleNegotiator(proxy: CGEventTapProxy,
-                                            type: CGEventType,
-                                            event: CGEvent,
-                                            userData: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
-    return SpiceKeyManager.shared.catchModifersEvent(event)
 }
